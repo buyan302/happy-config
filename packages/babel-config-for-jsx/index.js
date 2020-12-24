bfda4685cc14b1e7cde711b75a9c9c336a6693db
buyan302 <fuhao.xu@yitu-inc.com>
2020-12-24 19:10:56 +0800

    feat: babel-config-for-jsx

---
+++ packages/babel-config-for-jsx/index.js
@@ -0,0 +1,21 @@
+const hasJsxRuntime = function () {
+  try {
+    require.resolve('react/jsx-runtime');
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+module.exports = {
+  extends: 'babel-config-for-js',
+  overrides: [{
+    presets: [['@babel/preset-react', {
+      runtime: hasJsxRuntime ? 'automatic' : 'classic',
+    }]],
+    plugins: hasJsxRuntime() ? undefined : [
+      'transform-react-jsx-self',
+      'transform-react-jsx-source',
+    ],
+  }],
+};
